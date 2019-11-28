399aac685a5c39ddeafcac58b5e4e735bd78f573
Jay <niujay@gmail.com>
2019-11-28 18:55:09 +0800

    CUDNN problem resolved by adding GPU 'set_memory_growth'

--- Chap8/chap8-5.py
+++ Chap8/chap8-5.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 from tensorflow.keras import layers, Sequential
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 # 8.5.1 加载模型
 # 加载ImageNet预训练网络模型，并去掉最后一层
 resnet = tf.keras.applications.ResNet50(weights='imagenet', include_top=False)
@@ -14,7 +26,7 @@ print(out.shape)
 global_average_layer = layers.GlobalAveragePooling2D()
 # 利用上一层的输出作为本层的输入，测试其输出
 x = tf.random.normal([4, 7, 7, 2048])
-out = global_average_layer(x)   # 池化层降维
+out = global_average_layer(x)  # 池化层降维
 print(out.shape)
 
 # 新建全连接层
