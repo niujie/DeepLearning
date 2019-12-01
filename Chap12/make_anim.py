8a0051cf0049d82cf47fac9e85d32af2c475c96b
niujie <niujay@gmail.com>
2019-12-01 11:54:12 +0800

    updated

--- Chap12/make_anim.py
+++ Chap12/make_anim.py
@@ -12,7 +12,6 @@ with imageio.get_writer(anim_file, mode='I') as writer:
     filenames = glob.glob('sampled_epoch*.png')
     filenames = sorted(filenames)
     filenames.sort(key=lambda x: int(x[13:-4]))  # 文件名按数字排序
-    last = -1
     for i, filename in enumerate(filenames):
         image = imageio.imread(filename)
         writer.append_data(image)
